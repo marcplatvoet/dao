dbba50e2e745d2cdeb5602a8dde298352a7c128e
marcplatvoet <marc.platvoet@gmail.com>
2021-06-29 23:00:10 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    env: {
-        mongodburl: "mongodb+srv://marcplatvoet:QbihGVliE0YQqqPzxuHgdd@cluster0.83hgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    }
-};
